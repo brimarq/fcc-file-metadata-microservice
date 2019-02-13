227ec5d192a3234336c42fee24ab6a6a7783c183
Brian Marquis <rbmarquis.dev@gmail.com>
2019-02-12 19:37:07 -0600

    Post route for file upload. Added 404 handler.

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
+// Store uploaded files in memory https://github.com/expressjs/multer#memorystorage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+// Pass the upload.single function as inline middleware
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({filename: req.file.originalname, bytes: req.file.size});
+});
+
+// 404 / Not found handler http://expressjs.com/en/starter/faq.html
+app.use((req, res, next) => res.status(404).send("Not found."));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
